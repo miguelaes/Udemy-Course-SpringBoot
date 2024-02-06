5bc51196c758511f6055dfcdef79cc7e1399cf84
Maes <miguel.enriquezserrano@cognizant.com>
2024-02-06 15:45:51 +0100

    set up REST controller

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -3,12 +3,12 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //set up rest controller
 public class FunRestController {
     
     // expose "/" that return "Hello World"
     
-    @GetMapping("/")
+    @GetMapping("/") //handle HTTP GET requests
     public String sayHello() {
         return "Hello World!";
     }
